6afe955d0702c484ba13e44ccb0931fc246588f5
Abhay Kumar <shindheabhay@gmail.com>
2023-08-28 22:00:01 +0530

    to be committed:

---
+++ main.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".text", {
+   strings: ["Frontend Developer", "Web Developer","Web Designer"], // An array of strings
+   typeSpeed:20,
+   backSpeed:20,
+   backDelay:1000,
+   loop: true
+});
