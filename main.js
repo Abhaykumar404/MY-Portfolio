624153a6b452b426340b4a1f20d71f6e9e2990f7
Abhay Kumar <shindheabhay@gmail.com>
2023-08-31 15:10:20 +0530

    second commit

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 var typed = new Typed(".text", {
-   strings: ["Frontend Developer", "Web Developer","Web Designer"], // An array of strings
+   strings: ["Frontend Developer", "Web Developer", "C++ Programmer"], // An array of strings
    typeSpeed:20,
    backSpeed:20,
    backDelay:1000,
