e685342b3b8007baf688fc4abfa7bf5e80d1a271
Abhay Kumar <shindheabhay@gmail.com>
2023-09-10 17:46:35 +0530

    modify code

--- main.js
+++ main.js
@@ -5,3 +5,35 @@ var typed = new Typed(".text", {
    backDelay:1000,
    loop: true
 });
+
+// Get references to the button and the navigation menu
+const toggleButton = document.getElementById("toggleButton");
+const navMenu = document.getElementById("navMenu");
+
+// Function to hide the navigation menu
+function hideNavMenu() {
+   navMenu.classList.remove("active");
+}
+
+// Add a click event listener to the button
+toggleButton.addEventListener("click", function (event) {
+   // Toggle the "active" class on the navigation menu to show/hide it
+   navMenu.classList.toggle("active");
+   event.stopPropagation(); // Prevent the click event from propagating to document
+});
+
+// Add a click event listener to the document to hide the menu when clicking outside
+document.addEventListener("click", function (event) {
+   const target = event.target;
+
+   // Check if the click target is not the navigation menu or the toggle button
+   if (!navMenu.contains(target) && target !== toggleButton) {
+      hideNavMenu();
+   }
+});
+
+// Add click event listeners to each link to hide the menu when a link is clicked
+const links = navMenu.querySelectorAll("div a");
+links.forEach(function (link) {
+   link.addEventListener("click", hideNavMenu);
+});
